c2f98f50d7d49fd4a1f5834d6e61a4678afa1fe5
sydney-opyrchal <53274496+sydney-opyrchal@users.noreply.github.com>
2021-04-14 08:53:20 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
+import Teams from "./views/Teams/Teams"
+import Calendr from "./views/Calendr/Calendr"
+import FitnessTips from "./views/FitnessTips/FitnessTips"
+import HealthTips from "./views/HealthTips/HealthTips"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
 
@@ -15,6 +19,10 @@ const App = () => {
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
+        <Route exact path="/Teams" component={Teams} />
+        <Route exact path="/Calendr" component={Calendr} />
+        <Route exact path="/FitnessTips" component={FitnessTips} />
+        <Route exact path="/HealthTips" component={HealthTips} />
         <Route component={NotFound}/>
       </Switch>
     </div>
