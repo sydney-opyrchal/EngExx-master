6e7d5d9691cbfa1dc8ddf13f32332c5e1548cf0d
sydney-opyrchal <53274496+sydney-opyrchal@users.noreply.github.com>
2021-04-15 09:49:12 -0400

    Add files via upload

---
+++ client/src/views/CDC/CDC.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+
+/*import logo from '../../assets/logo.svg';*/
+import logo from '../../assets/HT.png';
+import './CDC.css';
+
+function CDC() {
+    
+    return (
+        
+        
+        <div className="App">
+              
+              
+
+            <header className="App-header"> 
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+                
+            </header>
+            
+            <h1>Da CDC Page</h1>
+
+            <p> </p>
+            <p> </p>
+        <div class="description">
+            <h2>Health Tippies</h2>
+            <p> - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.
+            </p>
+            <p>    
+                - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.
+                </p>
+                <p>
+                -Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.
+
+            </p>
+        </div>
+
+        
+
+
+       
+        
+            <h2>Questions</h2>
+            
+        
+        
+    
+    </div> 
+    
+    
+       
+       
+        
+
+        
+    );
+}
+
+export default CDC;
