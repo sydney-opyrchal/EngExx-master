1746a46bb5953e8c00fe16e164673183af06ea32
sydney-opyrchal <53274496+sydney-opyrchal@users.noreply.github.com>
2021-04-16 15:11:32 -0400

    Add files via upload

--- client/src/views/Login/login.js
+++ client/src/views/Login/login.js
@@ -28,9 +28,7 @@ export default class Login extends Component {
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
+                
             </form>
         );
     }
