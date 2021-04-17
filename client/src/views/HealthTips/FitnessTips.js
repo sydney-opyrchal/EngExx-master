f2b13f2eb97afeb9f1d0c42d55fa824337e73092
sydney-opyrchal <53274496+sydney-opyrchal@users.noreply.github.com>
2021-04-16 20:26:07 -0400

    Add files via upload

---
+++ client/src/views/HealthTips/FitnessTips.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+
+/*import logo from '../../assets/logo.svg';*/
+import logo from '../../assets/HT.png';
+import './HealthTips.css';
+
+function HealthTips() {
+    
+    return (
+        
+        
+        <div className="App">
+              
+              
+
+            <header className="App-header"> 
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+                
+            </header>
+            
+            <h1>Da Fitness Page</h1>
+
+            <p> </p>
+            <p> </p>
+        <div class="description">
+            <h2>Fitness Tippies</h2>
+            <p> - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.
+            </p>
+            <p>    
+                - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.
+                </p>
+                <p>
+                -Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.
+
+            </p>
+        </div>
+
+        
+
+
+       
+        
+            <h2>Questions</h2>
+            
+        
+        
+    <div class="form-group">
+        <input type="text" class="form-control" placeholder="Name" name=""/>
+    </div>
+    <div class="form-group">
+        <input type="email" class="form-control" placeholder="Email Address" name="email"/>
+    </div>
+    <div class="form-group">
+        <textarea class="form-control" rows="4"></textarea>
+    </div>
+    <input type="submit" class="btn btn-secondary btn-block" value="Send" name=""></input>
+    </div> 
+    
+    
+       
+       
+        
+
+        
+    );
+}
+
+export default HealthTips;
