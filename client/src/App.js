7db5623fa379e33f54f58dd7676834f0ef0a3fbc
sydney-opyrchal <53274496+sydney-opyrchal@users.noreply.github.com>
2021-04-17 19:00:38 -0400

    app

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
+import Teams from "./views/Teams/Teams"
+import TheCalendar from "./views/TheCalendar/TheCalendar"
+
+import FitnessTips from "./views/FitnessTips/FitnessTips"
+import HealthTips from "./views/HealthTips/HealthTips"
+import CDC from "./views/CDC/CDC"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
+import Login from "./views/Login/login"
 
 
 const App = () => {
@@ -15,6 +22,13 @@ const App = () => {
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
+        <Route exact path="/Teams" component={Teams} />
+        <Route exact path="/Calendar" component={TheCalendar} /> 
+        <Route exact path="/FitnessTips" component={FitnessTips} />
+        <Route exact path="/HealthTips" component={HealthTips} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/CDCTips" component={CDC} />
+        
         <Route component={NotFound}/>
       </Switch>
     </div>
