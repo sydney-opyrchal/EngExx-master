8e28bd752318d1c5fd003d792e7be57be58a0f06
Benjamin Rodriguez <brodriguez3@ufl.edu>
2021-04-20 02:58:48 -0400

    alien contact
    we have established connection with the mongodb server

--- connectMongodb.js
+++ connectMongodb.js
@@ -1,20 +1,49 @@
-"use strict";
+//import config from "./config";
+const {MongoClient} = require('mongodb');
 
-/*import all libraries */
-import config from "./config.js";
-import mongoose from "mongoose";
 
-/* Connect to your database using mongoose */
-const connectToDatabase = () => {
-  const link = config.db.uri;
-  //see https://mongoosejs.com/docs/connections.html
-  //See https://docs.atlas.mongodb.com/driver-connection/
-  mongoose
-    .connect(link, { useNewUrlParser: true, useUnifiedTopology: true})
-    .catch((error) => console.error(error));
-  mongoose.set("useCreateIndex", true);
-  mongoose.set("useFindAndModify", false);
-  return mongoose.connection;
-};
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://brUser:gp6unZWVYat4CbSv@cluster0.myko0.mongodb.net/test";
 
-export { connectToDatabase };
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+/*
+        await addUserToSchema(client, 
+          {
+            username: "admin",
+            password: "admin",
+            email: "admin",
+            isAdmin: true
+          });*/
+      
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+async function addUserToSchema(client, newUser){
+  const result = await client.db("database").collection("login").insertOne(newUser);
+  console.log('New user created with the following username: ', newUser.username);
+}
+
+main().catch(console.error);
